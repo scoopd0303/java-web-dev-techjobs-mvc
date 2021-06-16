5b8058483473ae597d20b9399a19a0c5fc1b921d
Scott McDonald <scottmcdonald@Scotts-MacBook-Air.local>
2021-06-16 13:00:29 -0500

    added TechJobsController to store the actionChoices and columnChoices HashMaps and extended it to all other controllers

---
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/TechJobsController.java
@@ -0,0 +1,27 @@
+package org.launchcode.javawebdevtechjobsmvc.controllers;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.HashMap;
+
+public class TechJobsController {
+
+    static HashMap<String, String> actionChoices = new HashMap<>();
+    static HashMap<String, Object> columnChoices = new HashMap<>();
+
+    public TechJobsController() {
+        actionChoices.put("search", "Search");
+        actionChoices.put("list", "List");
+
+        columnChoices.put("all", "All");
+        columnChoices.put("employer", "Employer");
+        columnChoices.put("location", "Location");
+        columnChoices.put("positionType", "Position Type");
+        columnChoices.put("coreCompetency", "Skill");
+    }
+
+    @ModelAttribute("actions")
+    public static HashMap<String, String> getActionChoices() {
+        return actionChoices;
+    }
+}
